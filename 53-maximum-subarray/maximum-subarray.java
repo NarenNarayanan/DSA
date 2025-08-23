1560ef7eb1cc275da0d2d397e8bf3c157590107e
Naren Narayanan P S <2023103606@student.annauniv.edu>
2025-08-23 15:54:15 +0530

    Time: 1 ms (99.54%) | Memory: 57.3 MB (31.12%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0,max=nums[0];
+       for(int i=0;i<nums.length;i++){
+        if(sum<0)sum=0;
+        sum=sum+nums[i];
+        max=Math.max(max,sum);
+       }
+       return max;
+    }
+}
