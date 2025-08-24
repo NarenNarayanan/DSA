d4a0611041c0b99234c7c3a02a7afea1da63852b
Naren Narayanan P S <2023103606@student.annauniv.edu>
2025-08-24 13:01:51 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (37.29%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,18 +1,18 @@
 class Solution {
+    public void help(int[] nums, int start, int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
     public void rotate(int[] nums, int k) {
-        int n=nums.length;
-        if(k==n)return;
-        k=k%n;
-        int m=0;
-        int [] help=new int[n];
-        for(int i=n-k;i<n;i++){
-            help[m++]=nums[i];
-        }
-        for(int i=0;i<n-k;i++){
-            help[m++]=nums[i];
-        }
-        for(int i=0;i<n;i++){
-            nums[i]=help[i];
-        }
+       int n=nums.length;
+       k=k%n;
+       help(nums,0,n-1);
+       help(nums,0,k-1);
+       help(nums,k,n-1);
     }
 }
