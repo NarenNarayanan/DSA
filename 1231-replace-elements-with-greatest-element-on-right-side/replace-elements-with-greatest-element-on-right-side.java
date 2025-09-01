cdb7db92005811ab6864ef533180bc4f12f521e7
Naren Narayanan P S <2023103606@student.annauniv.edu>
2025-09-01 20:48:34 +0530

    Time: 3 ms (53.65%) | Memory: 46.9 MB (34.97%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n=arr.length;
+        int max=arr[n-1];
+        arr[n-1]=-1;
+        if(n<=1)return arr;
+        for(int i=n-2;i>=0;i--){
+            int temp=arr[i];
+            arr[i]=max;
+            max=Math.max(max,temp);
+        }
+        return arr;
+    }
+}
