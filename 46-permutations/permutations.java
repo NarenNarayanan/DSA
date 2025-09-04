5d49d84259cead2f549b477b5b000ca2601e28e3
Naren Narayanan P S <2023103606@student.annauniv.edu>
2025-09-05 00:11:14 +0530

    Time: 4 ms (3.74%) | Memory: 44.5 MB (82.52%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void help(int[] nums,boolean[] arr,List<List<Integer>> result,List<Integer> ds){
+        if(ds.size()==nums.length){
+            result.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!arr[i]){
+                arr[i]=true;
+                ds.add(nums[i]);
+                help(nums,arr,result,ds);
+                ds.remove(ds.size()-1);
+                help(nums,arr,result,ds);
+                arr[i]=false;
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        boolean[] arr=new boolean[nums.length];
+        List<List<Integer>> result=new ArrayList<>();
+        help(nums,arr,result,new ArrayList<>());
+        return result;
+    }
+}
