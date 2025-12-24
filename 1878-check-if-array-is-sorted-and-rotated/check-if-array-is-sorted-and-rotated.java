9306086987d32c078b4c65576b9fa063484ebedb
Naren Narayanan P S <2023103606@student.annauniv.edu>
2025-12-24 15:05:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (35.23%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -2,9 +2,7 @@ class Solution {
     public boolean check(int[] nums) {
         int dec=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]>nums[(i+1)%nums.length]){
-                dec++;
-            }
+            if(nums[i]>nums[(i+1)%nums.length])dec++;
             if(dec>1)return false;
         }
         return true;
