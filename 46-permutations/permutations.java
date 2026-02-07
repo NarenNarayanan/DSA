2d6e93f6ae2ca7a7dd7e264239938925c0fd3d4e
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-02-07 09:56:07 +0530

    Time: 1 ms (86.96%) | Memory: 45.5 MB (77.66%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,23 @@
 class Solution {
-    public void help(int[] nums,boolean[] arr,List<List<Integer>> result,List<Integer> ds){
-        if(ds.size()==nums.length){
-            result.add(new ArrayList<>(ds));
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result =new ArrayList<>();
+        boolean[] arr=new boolean[nums.length];
+        backtrack(arr,nums,result,new ArrayList<>());
+        return result;
+    }
+    public void backtrack(boolean[] arr,int[] nums,List<List<Integer>> result,List<Integer> list){
+        if(list.size()==nums.length){
+            result.add(new ArrayList(list));
             return;
         }
         for(int i=0;i<nums.length;i++){
             if(!arr[i]){
                 arr[i]=true;
-                ds.add(nums[i]);
-                help(nums,arr,result,ds);
-                ds.remove(ds.size()-1);
+                list.add(nums[i]);
+                backtrack(arr,nums,result,list);
+                list.remove(list.size()-1);
                 arr[i]=false;
             }
         }
-    }
-    public List<List<Integer>> permute(int[] nums) {
-        boolean[] arr=new boolean[nums.length];
-        List<List<Integer>> result=new ArrayList<>();
-        help(nums,arr,result,new ArrayList<>());
-        return result;
     }
 }
