6e36772e289941da3be06eed4a09eb33d6a49289
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-05-26 14:04:51 +0530

    Time: 1 ms (99.96%) | Memory: 46.8 MB (80.41%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       int temp=nums[0];
-       for(int i=1;i<nums.length;i++){
-        temp^=nums[i];
-       }
-       return temp;
+        int ans = nums[0];
+        for(int i =1; i<nums.length;i++){
+            ans =ans ^ nums[i];
+        }
+        return ans;
     }
 }
