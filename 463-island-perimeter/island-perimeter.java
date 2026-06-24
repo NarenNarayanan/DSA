af6f946b04d0c0e2f958991ead094063efc41a9d
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-06-24 14:33:50 +0530

    Time: 6 ms (30.56%) | Memory: 47.3 MB (37.15%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int dfs(int row,int col,int[][] grid,int[][] visited){
+    public int dfs(int row,int col,int[][] grid){
         if(row<0 || col<0 || row>=grid.length || col>=grid[0].length)return 1;
-        if(visited[row][col]==-1)return 1;
-        if(visited[row][col]==1)return 0;
-        visited[row][col]=1;
+        if(grid[row][col]==0)return 1;
+        if(grid[row][col]==-1)return 0;
+        grid[row][col]=-1;
         int perimeter=0;
-        perimeter+=dfs(row+1,col,grid,visited);
-        perimeter+=dfs(row-1,col,grid,visited);
-        perimeter+=dfs(row,col+1,grid,visited);
-        perimeter+=dfs(row,col-1,grid,visited);
+        perimeter+=dfs(row+1,col,grid);
+        perimeter+=dfs(row-1,col,grid);
+        perimeter+=dfs(row,col+1,grid);
+        perimeter+=dfs(row,col-1,grid);
         return perimeter;
     }
     public int islandPerimeter(int[][] grid) {
         int m=grid.length , n=grid[0].length;
-        int[][] visited=new int[m][n];
-        int i,j,x=0,y=0;
-        for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
-                if(grid[i][j]==1){x=i;y=j;}
-                if(grid[i][j]==0)visited[i][j]=-1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    return dfs(i,j,grid);
+                }
             }
         }
-        return dfs(x,y,grid,visited);
+        return 0;
     }
 }
