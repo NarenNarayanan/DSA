13bea13406687da81c9154385c3b3f2575a2e208
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-06-30 16:39:46 +0530

    Time: 16 ms (25.39%) | Memory: 69.3 MB (39.14%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,19 +8,11 @@
  * }
  */
 class Solution {
-    TreeNode ans=null;
-    public boolean foundNode(TreeNode root, TreeNode p, TreeNode q){
-        if(root==null)return false;
-        if(root==p || root==q){ans=root; return true;}
-        boolean foundLeft=foundNode(root.left,p,q);
-        boolean foundRight=foundNode(root.right,p,q);
-        if(foundLeft && foundRight){
-            ans=root;
-        }
-        return foundLeft || foundRight;
-    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        foundNode(root,p,q);
-        return ans;
+        if(root==null || root==p || root==q)return root;
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null && right!=null)return root;
+        return left!=null ? left : right;
     }
 }
