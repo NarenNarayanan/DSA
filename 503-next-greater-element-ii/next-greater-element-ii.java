be482fa773f7ad75fa3e21c43b27f0f9312e2ef3
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-02 14:21:41 +0530

    Time: 15 ms (60.54%) | Memory: 48.9 MB (12.71%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int[] ans=new int[nums.length];
+        int cnt=0;
+        Arrays.fill(ans,-1);
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<nums.length;i=(i+1)%nums.length){
+            while(!stack.isEmpty() && nums[i]>nums[stack.peek()]){
+                ans[stack.pop()]=nums[i];
+            }
+            stack.push(i);
+            cnt++;
+            if(cnt==nums.length*2)break;
+        }
+        return ans;
+    }
+}
