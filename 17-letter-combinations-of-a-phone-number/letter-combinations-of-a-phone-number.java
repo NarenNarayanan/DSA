a7476a32838de186012f31113e46d2fa57229241
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-12 20:22:38 +0530

    Time: 3 ms (32.48%) | Memory: 49.3 MB (7.77%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,31 +1,18 @@
 class Solution {
-    public List<String> letterCombinations(String digits) {
-        List<String> result=new ArrayList<>();
-        if (digits=="" || digits==null){
-            return result;
-        }
-        Map<Character,String> map=new HashMap<>();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
-        backtrack(0,digits,map,new StringBuilder(),result);
-        return result;
-    }
-    public void backtrack(int i,String digits,Map<Character,String> map,StringBuilder curr,List<String> result){
-        if(i==digits.length()){
-            result.add(curr.toString());
+    public void help(String digits,String[] phone,List<String> ans,String str,int index){
+        if(index==digits.length()){
+            ans.add(str);
             return;
         }
-        String letters=map.get(digits.charAt(i));
-        for(char c: letters.toCharArray()){
-            curr.append(c);
-            backtrack(i+1,digits,map,curr,result);
-            curr.deleteCharAt(curr.length()-1);
+        String temp=phone[digits.charAt(index)-'0'];
+        for(int i=0;i<temp.length();i++){
+            help(digits,phone,ans,str+temp.charAt(i),index+1);
         }
+    }
+    public List<String> letterCombinations(String digits) {
+        String[] phone = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> ans=new ArrayList<>();
+        help(digits,phone,ans,"",0);
+        return ans;
     }
 }
