d64dfcc1b78f4036cb0ec3829acdce07479f788d
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-14 11:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (94.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,12 @@
 class Solution {
-    public int help(int n,int[] dp){
-        if(dp[n]!=-1)return dp[n];
-        dp[n]=help(n-1,dp)+help(n-2,dp);
-        return dp[n];
-    }
     public int climbStairs(int n) {
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
         dp[0]=1;
         dp[1]=1;
-        return help(n,dp);
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
