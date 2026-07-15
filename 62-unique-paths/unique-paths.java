9175b11f1aa5f99f7565b12b620dacac3b33effe
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-15 23:31:21 +0530

    Time: 1 ms (14.69%) | Memory: 42.5 MB (13.01%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,12 +1,4 @@
 class Solution {
-    public int help(int row,int col,int m,int n,int[][] dp){
-        if(row>=m || col>=n)return 0;
-        if(row==m-1 && col==n-1)return 1;
-        if(dp[row][col]!=-1)return dp[row][col];
-        int right=help(row,col+1,m,n,dp);
-        int down=help(row+1,col,m,n,dp);
-        return dp[row][col]=right+down;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n+1];
         dp[m-1][n-1]=1;
@@ -19,7 +11,6 @@ class Solution {
                     right = dp[row][col+1];
                 if(row+1 < m)
                     down = dp[row+1][col];
-
                 dp[row][col] = right + down;
             }
         }
