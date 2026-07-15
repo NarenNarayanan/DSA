ab7944cc38532913696ade960ede435ab0fe4a98
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-16 01:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (27.03%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int help(int[][] mat,int row,int col,int[][] dp){
-        int m=mat.length;
-        int n=mat[0].length;
-        if(row>=m || col>=n)return 0;
-        if(row==m-1 && col==n-1)return 1;
-        if(mat[row][col]==1)return 0;
-        if(dp[row][col]!=-1)return dp[row][col];
-        int right=help(mat,row,col+1,dp);
-        int left=help(mat,row+1,col,dp);
-        return dp[row][col]=right+left;
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         if(obstacleGrid[m-1][n-1]==1)return 0;
         int[][] dp=new int[m][n+1];
-        for(int[] arr : dp){
-            Arrays.fill(arr,-1);
+        dp[m-1][n-1]=1;
+        for(int row=m-1;row>=0;row--){
+            for(int col=n-1;col>=0;col--){
+                if(obstacleGrid[row][col]==1){
+                    dp[row][col]=0;
+                    continue;
+                }
+                if(row==m-1 && col==n-1)continue;
+                int right=0,left=0;
+                if(col+1 < n)right=dp[row][col+1];
+                if(row+1 < m)left=dp[row+1][col];
+                dp[row][col]=right+left;
+            }
         }
-        return help(obstacleGrid,0,0,dp);
+        return dp[0][0];
     }
 }
