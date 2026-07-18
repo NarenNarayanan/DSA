0b1aa449f1f7fe5a56dd24bdf085000156e4fd9a
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-18 17:19:08 +0530

    Time: 652 ms (5.68%) | Memory: 43.1 MB (70.18%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int rec(int[] nums,int index,int currSum,int target){
+    public int help(int[] nums,int target,int index){
+        if(index>nums.length)return 0;
         if(index==nums.length){
-            if(currSum==target)return 1;
+            if(target==0)return 1;
             else return 0;
         }
-        int plus=rec(nums,index+1,currSum+nums[index],target);
-        int neg=rec(nums,index+1,currSum-nums[index],target);
-        return plus+neg;
+        int pos=help(nums,target-nums[index],index+1);
+        int neg=help(nums,target+nums[index],index+1);
+        return pos+neg;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return rec(nums,0,0,target);
+        return help(nums,target,0);
     }
 }
