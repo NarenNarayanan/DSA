a5487acdf4e987be70f5036019ec06eb6613d6d4
Naren Narayanan P S <2023103606@student.annauniv.edu>
2026-07-31 13:14:53 +0530

    Time: 92 ms (64.54%) | Memory: 115.2 MB (56.17%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,55 +14,49 @@
  * }
  */
 class Solution {
-    public void buildParent(TreeNode root, TreeNode parent, Map<TreeNode, TreeNode> map) {
-        if (root == null)
-            return;
-        map.put(root, parent);
-        buildParent(root.left, root, map);
-        buildParent(root.right, root, map);
+    public void buildParent(TreeNode root,TreeNode parent,Map<TreeNode,TreeNode> map){
+        if(root==null)return;
+        map.put(root,parent);
+        buildParent(root.left,root,map);
+        buildParent(root.right,root,map);
     }
-
-    public TreeNode where(TreeNode root, int start) {
-        if (root == null)
-            return null;
-        if (root.val == start)
-            return root;
-        TreeNode left = where(root.left, start);
-        TreeNode right = where(root.right, start);
-        return left == null ? right : left;
+    public TreeNode where(TreeNode root,int start){
+        if(root==null)return null;
+        if(root.val==start)return root;
+        TreeNode left=where(root.left,start);
+        TreeNode right=where(root.right,start);
+        return left==null ? right : left;
     }
-
     public int amountOfTime(TreeNode root, int start) {
-        if (root == null)
-            return 0;
-        TreeNode target = where(root, start);
-        int time = 0;
-        Map<TreeNode, TreeNode> map = new HashMap<>();
-        buildParent(root, null, map);
-        Set<TreeNode> visited = new HashSet<>();
-        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null)return 0;
+        TreeNode target=where(root,start);
+        int time=0;
+        Map<TreeNode,TreeNode> map=new HashMap<>();
+        buildParent(root,null,map);
+        Set<TreeNode> visited=new HashSet<>();
+        Queue<TreeNode> q=new LinkedList<>();
         q.add(target);
         visited.add(target);
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                if (node.left != null && !visited.contains(node.left)) {
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                if(!visited.contains(node.left) && node.left!=null){
                     q.add(node.left);
                     visited.add(node.left);
                 }
-                if (node.right != null && !visited.contains(node.right)) {
+                if(!visited.contains(node.right) && node.right!=null){
                     q.add(node.right);
                     visited.add(node.right);
                 }
-                TreeNode parent = map.get(node);
-                if (!visited.contains(parent) && parent != null) {
+                TreeNode parent=map.get(node);
+                if(!visited.contains(parent) && parent!=null){
                     q.add(parent);
                     visited.add(parent);
                 }
             }
             time++;
         }
-        return time - 1;
+        return time-1;
     }
 }
